abcc36bf432613d1cbe8d2cd5efb23ae15217ca0
twubas <twubas@gmail.com>
2022-01-03 22:40:02 -0300

    Adicionar arquivos de projeto.

---
+++ PrimeiroAPP/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace PrimeiroAPP
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var a = "Preciso ";
+            var b = "reaprender ";
+            var c = "com urgência!!";
+            Console.WriteLine(a+b+ c);
+        }
+    }
+}
